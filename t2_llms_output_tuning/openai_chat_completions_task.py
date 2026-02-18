6d85d60507096c6750672592bda81d97a26d701b
“Khshanovskyi” <pavlo_khshanovskyi@epam.com>
2026-02-18 16:14:03 +0200

    2. LLM output tuning task

--- t2_llms_output_tuning/openai_chat_completions_task.py
+++ t2_llms_output_tuning/openai_chat_completions_task.py
@@ -54,9 +54,8 @@ from t2_llms_output_tuning._main import run
 
 run(
     client=OpenAIChatCompletionsClient(model_name='gpt-4o'),
-    print_request=True,
-    print_only_content=False,
+    print_request=True, # Switch to False if you do not want to see the request in console
+    print_only_content=False, # Switch to True if you want to see only content from response
 
-    n=2,
-    stop=["5"]
+
 )
