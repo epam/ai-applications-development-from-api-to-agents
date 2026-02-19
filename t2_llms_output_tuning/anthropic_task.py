56d008f0aa187cab65d2eb6dc597cadb62bf6137
“Khshanovskyi” <pavlo_khshanovskyi@epam.com>
2026-02-19 12:52:29 +0200

    3. Content generation

--- t2_llms_output_tuning/anthropic_task.py
+++ t2_llms_output_tuning/anthropic_task.py
@@ -7,5 +7,5 @@ run(
     print_request=True, # Switch to False if you do not want to see the request in console
     print_only_content=False, # Switch to True if you want to see only content from response
 
-    max_output_tokens=16
+    max_tokens=16
 )
